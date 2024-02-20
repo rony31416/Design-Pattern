0f58c9eaaac6d505dd358b9a5648b68a19a02f38
rony31416 <ronymajumder1171@gmail.com>
2024-02-20 07:19:06 +0600

    New

---
+++ Prototype model pattern/ShapeExample/Rectangle.java
@@ -0,0 +1,29 @@
+package ShapeExample;
+
+public class Rectangle extends Shape {
+   
+	 
+	public int width;
+	public int height;
+	
+	public Rectangle() {
+		
+	}
+	
+	public Rectangle(Rectangle target) 
+	{
+		super(target) ;
+		if(target != null ) {
+			this.width = target.width;
+			this.height = target.height;
+		}
+	}
+	
+	
+	@Override
+	public Shape clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
